ee9832143fe9667d591939a05872717bb0f0aad4
Md Adnan <102144238+adnansyed101@users.noreply.github.com>
2023-03-30 20:02:54 +0600

    complete the challenge
    The qr code challenge from front-end mentor is completed.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      },
+      fontFamily: {
+        body: ['Outfit']
+      }
+    },
+  },
+  plugins: [],
+};
